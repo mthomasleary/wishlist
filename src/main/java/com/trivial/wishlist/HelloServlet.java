dbeaebfbc70081ef88aff082a50b9acb0f8e4c17
mthomasleary <mthomasleary@gmail.com>
2016-11-25 16:30:22 -0600

    initial commit

---
+++ src/main/java/com/trivial/wishlist/HelloServlet.java
@@ -0,0 +1,18 @@
+package com.trivial.wishlist;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        response.setStatus(HttpServletResponse.SC_OK);
+        RequestDispatcher view = request.getRequestDispatcher("index.html");
+        view.forward(request, response);
+    }
+}
